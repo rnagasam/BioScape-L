d39120af82f31db38a70e7ab1f9e2570f2d6a792
Ramana Nagasamudram <rnagasam@stevens.edu>
2019-04-06 16:05:16 -0400

    Alternate approach.
    `value' represents a pi-process.  We spawn one of these for
    each pi-process in our program.

--- pi.erl
+++ pi.erl
@@ -30,34 +30,58 @@ pick_random(Ls) ->
     Index = rand:uniform(length(Ls)),
     lists:nth(Index, Ls).
 
-make_channel(Name) ->
-    make_channel(Name, []).
+lookup(Key, Env) ->
+    case lists:keyfind(Key, 1, Env) of
+	{_, Val} -> Val;
+	_Else -> error({key_not_found, Key, Env})
+    end.
 
-make_channel(Name, Listeners) ->
+channel(Name, Listeners, MsgBox) ->
     receive
-	{ send, Msg } ->
-	    {_, Pid} = pick_random(Listeners),
-	    Pid ! { Msg };
-	{ recv, ProcName, ProcPid } ->
-	    Proc = { ProcName, ProcPid },
-	    make_channel(Name, [Proc | Listeners]);
-	{ inspect } ->
-	    io:format("Chan ~p: Listeners: ~w~n", [Name, Listeners])
+	{send, PName, Msg} ->
+	    case Listeners of
+		[] -> channel(Name, Listeners, 
+			      queue:in({self(), PName, Msg}, MsgBox));
+		_ -> {Q, QPid} = pick_random(Listeners),
+		     QPid ! {self(), PName, Msg},
+		     lists:delete({Q, QPid}, Listeners)
+	    end;
+	{recv, PName, PPid} ->
+	    case queue:is_empty(MsgBox) of
+		true -> channel(Name, [{PName, PPid}|Listeners], MsgBox);
+		false -> {value, Msg} = queue:out(MsgBox),
+			 PPid ! Msg
+	    end
     end,
-    make_channel(Name, Listeners).
+    channel(Name, Listeners, MsgBox).
 
-% build_process should return a function which takes an environment as
-% its argument.  This function represents a process in the
-% pi-calculus.
-build_process(_) ->
-    error(not_implemented).
+build_channels([]) ->
+    [];
+build_channels([C|Cs]) ->
+    CPid = spawn(?MODULE, channel, [C, [], queue:new()]),
+    [{C, CPid} | build_channels(Cs)].
 
-% eval: Program List * Process List * Channel List -> Atom
-eval([], _, _) ->
-    ok;
-eval([{define, P, Actions}|Prog], Procs, Chans) ->
-    eval(Prog, [{P, build_process(Actions)} | Procs], Chans);
-eval([{simul, P, Q}|Prog], Procs, Chans) ->
-    spawn(?MODULE, lists:keyfind(P, 1, Procs), [Chans]),
-    spawn(?MODULE, lists:keyfind(Q, 1, Procs), [Chans]),
-    eval(Prog, Procs, Chans).
+value([prog, Cs, Ps]) ->
+    InitEnv = build_channels(Cs) ++ Ps,
+    [spawn(?MODULE, value, [Name, P, InitEnv]) || {Name, P} <- Ps].
+
+value(Name, {null}, _) ->
+    io:format("Process ~p terminated.", [Name]),
+    exit(normal);
+value(Name, {send, Chan, Msg, P}, Env) ->
+    CPid = lookup(Chan, Env),
+    CPid ! {send, Name, Msg},
+    receive
+	{msg_sent} -> value(Name, P, Env)
+    end;
+value(Name, {recv, Chan, Bind, P}, Env) ->
+    CPid = lookup(Chan, Env),
+    CPid ! {recv, Name, self()},
+    receive
+	{CPid, _, Msg} ->
+	    value(Name, P, [{Bind, Msg} | Env])
+    end;
+value(_, {spawn, P}, Env) ->
+    Proc = lookup(P, Env),
+    spawn(?MODULE, value, [P, Proc, Env]),
+    exit(normal).
