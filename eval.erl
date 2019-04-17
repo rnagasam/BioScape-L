fe9265e5fc1f5cab17ba19b71df167f180bca8cb
Ramana Nagasamudram <rnagasam@stevens.edu>
2019-04-16 21:49:58 -0400

    Add `eval' module
    `build_process' is arguably more efficient than `value' (look at
    previous commits), because it performs syntactic analysis only
    once, except when the term is "spawn P1 ... Pn".

---
+++ eval.erl
@@ -0,0 +1,51 @@
+-module(eval).
+-export([lookup/2, build_process/2, eval/3]).
+
+eval(Name, P, InitEnv) ->
+    (build_process(Name, P))(InitEnv).
+
+lookup(Key, Env) ->
+    case lists:keyfind(Key, 1, Env) of
+	{ _, Val } ->
+	    Val;
+	_Else ->
+	    error({key_not_found, Key, Env})
+    end.
+
+build_process(Name, { null }) ->
+    fun (_) ->
+	    io:format("Process ~p terminated.~n", [Name])
+    end;
+build_process(Name, { send, Chan, Msg, P }) ->
+    PProc = build_process(Name, P),
+    fun (Env) ->
+	    CPid = lookup(Chan, Env),
+	    CPid ! { send, Name, self(), Msg },
+	    io:format("Process ~p sent message ~p on chan ~p.~n",
+		      [Name, Msg, Chan]),
+	    receive
+		{ msg_sent } ->
+		    PProc(Env)
+	    end
+    end;
+build_process(Name, { recv, Chan, Bind, P }) ->
+    PProc = build_process(Name, P),
+    fun (Env) ->
+	    CPid = lookup(Chan, Env),
+	    CPid ! { recv, Name, self() },
+	    io:format("Process ~p waiting to recv message on chan ~p.~n",
+		      [Name, Chan]),
+	    receive
+		{ CPid, _, Msg } ->
+		    io:format("Process ~p recv'd message ~p on chan ~p.~n",
+			      [Name, Msg, Chan]),
+		    PProc([{ Bind, Msg } | Env])
+	    end
+    end;
+build_process(Name, { spawn, Ps }) ->
+    fun (Env) ->
+	    Procs = lists:map(fun (X) -> lookup(X, Env) end, Ps),
+	    [spawn(?MODULE, eval, [P, Proc, Env])
+	     || { P, Proc } <- lists:zip(Ps, Procs)],
+	    io:format("Process ~p spawn'd processes ~p.~n", [Name, Ps])
+    end.
--- pi.erl
+++ pi.erl
@@ -17,48 +17,9 @@ freevars({repeat, P}) ->
 fv(Expr) ->
     sets:to_list(freevars(Expr)).
 
-lookup(Key, Env) ->
-    case lists:keyfind(Key, 1, Env) of
-	{_, Val} -> Val;
-	_Else -> error({key_not_found, Key, Env})
-    end.
-
-build_channels([]) ->
-    [];
-build_channels([C|Cs]) ->
-    CPid = spawn(channel, channel, [C, [], queue:new()]),
-    [{C, CPid} | build_channels(Cs)].
-
 value([prog, Cs, Ps]) ->
-    InitEnv = build_channels(Cs) ++ Ps,
-    [spawn(?MODULE, value, [Name, P, InitEnv]) || {Name, P} <- Ps].
-
-value(Name, {null}, _) ->
-    io:format("Process ~p terminated.~n", [Name]),
-    exit(normal);
-value(Name, {send, Chan, Msg, P}, Env) ->
-    CPid = lookup(Chan, Env),
-    CPid ! {send, Name, self(), Msg},
-    io:format("Process ~p sent message ~p on chan ~p.~n", [Name, Msg, Chan]),
-    receive
-	{msg_sent} -> value(Name, P, Env)
-    end;
-value(Name, {recv, Chan, Bind, P}, Env) ->
-    CPid = lookup(Chan, Env),
-    CPid ! {recv, Name, self()},
-    io:format("Process ~p waiting to recv message on chan ~p.~n",
-	      [Name, Chan]),
-    receive
-	{CPid, _, Msg} ->
-	    io:format("Process ~p recv'd message ~p on chan ~p.~n",
-		      [Name, Msg, Chan]),
-	    value(Name, P, [{Bind, Msg} | Env])
-    end;
-value(Name, {spawn, Ps}, Env) ->
-    Procs = lists:map(fun(X) -> lookup(X, Env) end, Ps),
-    [spawn(?MODULE, value, [P, Proc, Env]) || {P, Proc} <- lists:zip(Ps, Procs)],
-    io:format("Process ~p spawn'd processes ~p.~n", [Name, Ps]),
-    exit(normal).
+    InitEnv = channel:build_channels(Cs) ++ Ps,
+    [spawn(eval, eval, [Name, P, InitEnv]) || { Name, P } <- Ps].
 
 test_prog() ->
     [prog, [a], [{p, {send, a, ack, {null}}}, {q, {recv, a, x, {null}}}]].
@@ -69,3 +30,6 @@ test_prog1() ->
 		    {procB, {recv, a, ack, {send, b, ack, {spawn, [procC]}}}},
 		    {procC, {recv, b, ack, {spawn, [procA, procB]}}}]].
 
+test_prog2() ->
+    [prog, [a], [{procA, {send, a, ack, {recv, a, ack, {null}}}},
+		 {procB, {recv, a, ack, {null}}}]].
