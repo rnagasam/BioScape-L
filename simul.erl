425a100d6371cdb4a3cc5d1b98cfcf091961f056
Ramana Nagasamudram <rnagasam@stevens.edu>
2019-04-29 21:33:57 -0400

    Interface for `simul'

--- simul.erl
+++ simul.erl
@@ -2,10 +2,24 @@
 -export([simul/2]).
 
 % For now, `simul' is used to cleanup channels after finishing a run.
-simul(Chans, 0) ->
+% `simul' additionally maintains a list of process info objects, where
+% each element is a tuple containing the processes Pid and location.
+% It is the responsibility of the process to update `simul' with its
+% location information.
+simul(Chans, 0, _ProcsInfo) ->
     [exit(C, kill) || { _, C } <- Chans];
-simul(Chans, N) ->
+simul(Chans, N, ProcsInfo) ->
     receive
-	{ done } -> simul(Chans, N-1);
-	{ create } -> simul(Chans, N+1)
+	{ done, ProcPid } ->
+	    io:format("Simul: ~p is done.~n", [ProcPid]),
+	    simul(Chans, N-1, dict:erase(ProcPid, ProcsInfo));
+	{ create, ProcPid, Location } ->
+	    simul(Chans, N+1, dict:store(ProcPid, Location, ProcsInfo));
+	{ update, ProcPid, Location } ->
+	    simul(Chans, N, dict:store(ProcPid, Location, ProcsInfo));
+	{ get_location, ProcPid, From } ->
+	    ProcLoc = dict:find(ProcPid, ProcsInfo),
+	    % `channel' should check for failure in recv'd message
+	    From ! { location, ProcLoc },
+	    simul(Chans, N, ProcsInfo)
     end.
