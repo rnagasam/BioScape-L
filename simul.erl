2c8c764800c86885230045b54c00ed7d90089b29
Ramana Nagasamudram <rnagasam@stevens.edu>
2019-05-03 18:55:48 -0400

    Maintain a simulation time

--- simul.erl
+++ simul.erl
@@ -2,34 +2,33 @@
 -export([simul/3]).
 -define(TIMEOUT, 5000).
 
-% For now, `simul' is used to cleanup channels after finishing a run.
-% `simul' additionally maintains a dictionary of process info objects,
-% with entity Pids as keys and locations as values.  It is the
-% responsibility of the process/entity to update `simul' with its
-% location information.
-simul(Chans, 0, ProcsInfo) ->
+simul(Chans, N, ProcsInfo) ->
+    simul(Chans, N, ProcsInfo, 0).
+
+simul(Chans, 0, ProcsInfo, Time) ->
     io:format("Simul: ~p~n", [dict:to_list(ProcsInfo)]),
+    io:format("Simulation: ran for ~p time steps~n", [Time]),
     dict:map(fun (Pid, _) -> exit(Pid, kill) end, ProcsInfo),
     [exit(C, kill) || { _, C } <- Chans];
-simul(Chans, N, ProcsInfo) ->
+simul(Chans, N, ProcsInfo, Time) ->
     receive
 	{ done, ProcPid } ->
 	    io:format("Simul: ~p is done.~n", [ProcPid]),
-	    simul(Chans, N-1, ProcsInfo);
-	    % simul(Chans, N-1, dict:erase(ProcPid, ProcsInfo));
+	    % simul(Chans, N-1, ProcsInfo, Time+1);
+	    simul(Chans, N-1, dict:erase(ProcPid, ProcsInfo), Time+1);
 	{ create, ProcPid, Location } ->
-	    simul(Chans, N+1, dict:store(ProcPid, Location, ProcsInfo));
+	    simul(Chans, N+1, dict:store(ProcPid, Location, ProcsInfo), Time+1);
 	{ update, ProcPid, Location } ->
-	    simul(Chans, N, dict:store(ProcPid, Location, ProcsInfo));
+	    simul(Chans, N, dict:store(ProcPid, Location, ProcsInfo), Time+1);
 	{ get_location, ProcPid, From } ->
 	    % `channel' should check for failure in recv'd message
 	    From ! dict:find(ProcPid, ProcsInfo),
-	    simul(Chans, N, ProcsInfo);
+	    simul(Chans, N, ProcsInfo, Time+1);
 	{ inspect_state } ->
 	    io:format("Simul: N: ~p, ProcsInfo: ~p~n",
 		      [N, dict:to_list(ProcsInfo)]),
-	    simul(Chans, N, ProcsInfo)
+	    simul(Chans, N, ProcsInfo, Time)
     after ?TIMEOUT ->
 	    io:format("Simul: exit"),
-	    simul(Chans, 0, ProcsInfo)
+	    simul(Chans, 0, ProcsInfo, Time)
     end.
